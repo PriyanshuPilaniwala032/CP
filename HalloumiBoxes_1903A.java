1d3374cc96c20aec90667c8575f8521ca1921ccf
PriyanshuPilaniwala032 <priyanshupilaniwala@gmail.com>
2026-05-03 22:52:32 +0530

    first sol

--- HalloumiBoxes_1903A.java
+++ HalloumiBoxes_1903A.java
@@ -1,8 +1,30 @@
 import java.util.*;
-class HalloumiBoxes1903A{
+class HalloumiBoxes_1903A{
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
-        
+        int t = scn.nextInt();
+
+        while(t > 0){
+            t--;
+            int n = scn.nextInt();
+            int k = scn.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++){
+                arr[i] = scn.nextInt();
+            }
+            if(k > 1){
+                System.out.println("YES");
+            }else{
+                boolean flag = false;
+                for(int i = 0; i < n - 1; i++){
+                    if(arr[i] > arr[i + 1]){
+                        flag = true;
+                        break;
+                    }
+                }
+                System.out.println(flag ? "NO" : "YES");
+            }
+        }
     }
 }
